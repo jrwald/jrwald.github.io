a88aee508ddb3edb3f33cd493225a56ce681a2cb
Jeff Wald <47639501+jrwald@users.noreply.github.com>
2021-12-02 10:34:56 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Template</title>
+    <link href="template.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <div id="wrap">
+    <header>
+      Header
+    </header>
+
+    <nav>
+      <?php include 'nav.html';?>
+    </nav>
+
+    <section>
+      Section
+    </section>
+
+    <footer>
+      Footer
+    </footer>
+  </div><!--Close wrap-->
+  </body>
+</html>
